5b78d242d107b7d90fc204a352d2bed69d549405
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-04-03 04:56:05 +0200

    ans

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 class Square:
-    """A class to represent a square."""
-
-    def __init__(self, side_length):
-        """
-        Initializes a square with the given side length.
-
-        Args:
-            side_length (int): The length of each side of the square.
-        """
-        self.side_length = side_length
+    pass
+    """
+    creates a square class object
+    """
