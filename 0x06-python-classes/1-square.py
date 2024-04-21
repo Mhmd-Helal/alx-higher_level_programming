5639f462b105a591eb953f600c8a3e1aff64d60a
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-04-22 00:11:20 +0200

    docstring

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
+'''module doc'''
+
+
 class Square:
     """
     creates a square class object
     """
     def __init__(self, size):
         self.__size = size
-    """
-    initializes a square object with parameters
-    args:
-        __size(int): size of the square
-    """
