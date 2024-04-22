6d269ece4f66d351b4eda428389384e5a2d1bf54
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-04-22 02:20:27 +0200

    docstring

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+'''module doc'''
+
+
 class Square:
+    ''' create Square class'''
 
     def __init__(self, size=0):
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
