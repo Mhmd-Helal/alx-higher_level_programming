2367afbe52fcc0bebfc831c12b23baf64275fcac
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-06-20 06:15:34 -0700

    ans

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const sizeSquare = process.argv[2];
+if (sizeSquare) {
+  for (let i = 0; i < sizeSquare; i++) {
+    let sympol = '';
+    for (let j = 0; j < sizeSquare; j++) {
+      sympol += 'X';
+    }
+    console.log(sympol);
+  }
+} else {
+  console.log('Missing size');
+}
