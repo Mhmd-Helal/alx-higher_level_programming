055319b424603f650ff023f95c6249bfb5b0cbce
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-06-20 05:26:42 -0700

    ans

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const x = process.argv[2];
+let i = 0;
+if (x) {
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
