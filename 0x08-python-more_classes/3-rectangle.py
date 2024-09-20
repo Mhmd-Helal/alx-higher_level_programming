31f635e5e541b54182f56138cd83fe48d948b9cc
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-09-19 22:50:44 -0700

    ans

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -44,7 +44,7 @@ class Rectangle:
 
     def __str__(self):
         if self.width == 0 or self.height == 0:
-            return None
+            return ""
         rectangle = []
         for _ in range(self.heigt):
             rectangle.append("#" * self.width)
