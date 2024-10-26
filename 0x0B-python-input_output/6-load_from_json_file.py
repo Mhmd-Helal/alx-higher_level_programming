32e8c51567aad7467ae2cc27d166cff19706407a
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-10-25 21:28:08 -0700

    ans

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Create object from a JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """ creates an Object from a “JSON file”"""
+    json.load(filename)
