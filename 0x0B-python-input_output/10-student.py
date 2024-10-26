7e325f781b0feb199c9e33d576a08344798cea69
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-10-26 09:30:07 -0700

    ans

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""""""
+
+
+class Student:
+    """defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance
+            If attrs is a list of strings, only attribute names contained
+            in this list must be retrieved.
+            Otherwise, all attributes must be retrieved
+        """
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            my_dict = {}
+            for i in self.__dict__:
+                if i in attrs:
+                    my_dict[i] = self.__dict__[i]
+            return my_dict
+            # return {x: y for x, y in self.__dict__.items() if x in attrs}
+        return self.__dict__
