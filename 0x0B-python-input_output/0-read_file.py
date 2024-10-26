3704e70ab1af2bab47b4ab8e27723f80bbdcfaba
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-10-25 20:40:01 -0700

    ans

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=""):
     """reads a text file and prints it to stdout"""
     with open(filename, 'r', encoding='UTF-8') as f:
-        print(f.read())
+        for line in f:
+            print(line, end='')
